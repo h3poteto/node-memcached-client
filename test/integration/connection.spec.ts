1c17d07af66b4cf64a0c7e85317a00f6689c5e34
AkiraFukushima <h3.poteto@gmail.com>
2019-10-01 23:43:22 +0900

    Change get method to get multiple values

--- test/integration/connection.spec.ts
+++ test/integration/connection.spec.ts
@@ -1,34 +1,44 @@
 import Memcached, { Connection, Metadata } from '@/index'
 
-describe('set', () => {
-  it('should be setted', async () => {
-    const client = new Memcached('127.0.0.1', 11211)
-    const conn: Connection = await client.connect()
-    const code = await conn.set('test_key', 'hoge', false, 0)
-    expect(code).toEqual('STORED')
+describe('connection', () => {
+  let client: Memcached
+  let conn: Connection
+  beforeEach(async () => {
+    client = new Memcached('127.0.0.1', 11211)
+    conn = await client.connect()
+  })
+
+  afterEach(async () => {
     await client.close()
   })
-})
 
-describe('get', () => {
-  it('should get', async () => {
-    const client = new Memcached('127.0.0.1', 11211)
-    const conn: Connection = await client.connect()
-    await conn.set('test_key', 'hoge', false, 0)
-    const data: Metadata | null = await conn.get('test_key')
-    expect(data).not.toBeNull
-    expect(data!.value).toEqual('hoge')
-    await client.close()
+  describe('set', () => {
+    it('should be setted', async () => {
+      const code = await conn.set('test_key', 'hoge', false, 0)
+      expect(code).toEqual('STORED')
+    })
   })
-})
 
-describe('delete', () => {
-  it('should be deleted', async () => {
-    const client = new Memcached('127.0.0.1', 11211)
-    const conn: Connection = await client.connect()
-    await conn.set('test_key', 'hoge', false, 0)
-    const code = await conn.delete('test_key')
-    expect(code).toEqual('DELETED')
-    await client.close()
+  describe('get', () => {
+    it('should get a key', async () => {
+      await conn.set('test_key', 'hoge', false, 0)
+      await conn.set('test_key_2', 'fuga', false, 0)
+      const data: { [key: string]: Metadata } = await conn.get('test_key')
+      expect(data['test_key'].value).toEqual('hoge')
+    })
+
+    it('should get multiple keys', async () => {
+      const data: { [key: string]: Metadata } = await conn.get('test_key', 'test_key_2')
+      expect(data['test_key'].value).toEqual('hoge')
+      expect(data['test_key_2'].value).toEqual('fuga')
+    })
+  })
+
+  describe('delete', () => {
+    it('should be deleted', async () => {
+      await conn.set('test_key', 'hoge', false, 0)
+      const code = await conn.delete('test_key')
+      expect(code).toEqual('DELETED')
+    })
   })
 })
